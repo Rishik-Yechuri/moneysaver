984700639f048677187934e939eb902044443c1b
sudoer777 <78781902+sudoer777@users.noreply.github.com>
2022-09-24 21:19:48 -0500

    Reorder files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './client/index.css';
+import App from './client/App';
+import reportWebVitals from './client/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
